b613b531f969d6cefb832f69e5d4e0b5b916e287
ritikakr <90204179+ritikakr@users.noreply.github.com>
2024-09-30 13:13:17 +0530

    Add files via upload

---
+++ Polymorphism/Protected.java
@@ -0,0 +1,12 @@
+package com.abstraction;
+import com.encapsulation.*;
+
+public class Protected extends Student
+{
+	public static void main(String[] args)
+	{
+		Protected p = new Protected();
+		p.mssg1();
+	}
+
+}
