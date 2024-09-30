3a81e3dbf124af101fde87d56dfd46cf8f9561b2
ritikakr <90204179+ritikakr@users.noreply.github.com>
2024-09-30 13:14:23 +0530

    Add files via upload

---
+++ AccessSpecifier/Main.java
@@ -0,0 +1,16 @@
+package com.access;
+
+public class Main 
+{
+
+	public static void main(String[] args) 
+	{
+		Circle c = new Circle();
+		c.area_circle(4);
+		
+		Rectangle r = new Rectangle();
+		r.area_rect(2.50, 4.50);
+					
+	}
+
+}
